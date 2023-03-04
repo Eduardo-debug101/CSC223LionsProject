fdb5232d4cc85ed33498dd54f17f3d55cdc1627b
Eduardo-debug101 <eja2770@email.vccs.edu>
2023-03-04 12:03:14 -0500

    Update StoreDriver.java

--- src/lionsCheckpointB/StoreDriver.java
+++ src/lionsCheckpointB/StoreDriver.java
@@ -8,11 +8,11 @@ public class StoreDriver {
 
     public static void main(String[] args) {
 
-        int minArrival = 1;
-        int maxArrival = 5;
+        int minArrival = 0;
+        int maxArrival = 3;
         int minService = 1;
-        int maxService = 7;
-        int numCustomers = 25;
+        int maxService = 20;
+        int numCustomers = 20;
 
         Simulator sim = new Simulator(minArrival, maxArrival, minService, maxService, numCustomers);
         sim.start();
