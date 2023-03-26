a8c2a6dc63aca9f4ff701c0a8b66198ed257b946
Eduardo-debug101 <eja2770@email.vccs.edu>
2023-03-25 23:10:45 -0400

    UML

--- src/lionsCheckpointC/StoreDriver.java
+++ src/lionsCheckpointC/StoreDriver.java
@@ -27,19 +27,18 @@ public class StoreDriver {
     }
 
     public static void main(String[] args) {
-
-        int minArrival = 1;
-        int maxArrival = 3;
-        int minService = 10;
-        int maxService = 30;
-        int numCustomers = 20;
-        int selfSlow = 0;
-//    	int minArrival = parseInt("Enter minimum arrival time between customers: ");
-//		int maxArrival = parseInt("Enter maximum arrival time between customers: ");
-//		int minService = parseInt("Enter minimum service time: ");
-//		int maxService = parseInt("Enter maximum service time: ");
-//		int numCustomers = parseInt("Number of customers to serve: ");
-//      int selfSlow = parseInt("Percentage of slowing for self checkouts (ex. 10 = 10%): ");
+//        int minArrival = 1;
+//        int maxArrival = 3;
+//        int minService = 10;
+//        int maxService = 30;
+//        int numCustomers = 20;
+//        int selfSlow = 0;
+    	int minArrival = parseInt("Enter minimum arrival time between customers: ");
+		int maxArrival = parseInt("Enter maximum arrival time between customers: ");
+		int minService = parseInt("Enter minimum service time: ");
+		int maxService = parseInt("Enter maximum service time: ");
+		int numCustomers = parseInt("Number of customers to serve: ");
+		int selfSlow = parseInt("Percentage of slowing for self checkouts (ex. 10 = 10%): ");
 
         Simulator sim = new Simulator(minArrival, maxArrival, minService, maxService, numCustomers, selfSlow);
         sim.start();
